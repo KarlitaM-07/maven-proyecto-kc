590fb88bdef885dcc624f7c0fde1a585e5cab869
Karla Chipantasi <belenchipantai@gmail.com>
2021-09-06 20:15:14 -0500

    Mi maven Auto

---
+++ src/main/java1/ejercicios/variables/locales/globales/Auto.java
@@ -0,0 +1,28 @@
+package ejercicios.variables.locales.globales;
+
+public class Auto {
+
+	
+	private String Dueño;
+	private int marca;
+	private int precio;
+	
+	
+	// Variables
+	// Globales
+	// Locales 
+	
+	public void nombre () {
+		this.Dueño= "Gio";
+		this.precio= 15000;
+		
+		precio = 15000;
+		precio= 15000;
+		
+				
+		String dueño = "Jorge";
+		dueño= "Jorge";
+		
+	}
+	
+}
