5b21350357b5de5b111310268d8fd0aeb252d4ea
Karla Chipantasi <belenchipantai@gmail.com>
2021-09-06 20:18:11 -0500

    Main Maven

---
+++ src/main/java1/ejercicios/clase/string/Main.java
@@ -0,0 +1,46 @@
+package ejercicios.clase.string;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1 contains
+		
+		String frase = "Programacion de algoritmos modulo II";
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Inngrese el nombre del libro");
+		String palabra = teclado.nextLine();
+		
+		boolean resultado = frase.contains(palabra);
+		System.out.println("Contiene la palabra: "+ resultado);
+		
+		
+		// 2 equals
+		
+		System.out.println("#+###################");
+		String cadena1= "edison";
+		System.out.println("Ingrese la segunda cadena a comparar");
+		String cadena2 = teclado.nextLine();
+		
+	
+		boolean resultado2 = cadena1.equals(cadena2);
+		System.out.println("Las cadenas son iguales: " + resultado);
+		
+		
+		//3.4. Upper Lower
+		
+		System.out.println("------------------------------");
+		String nombre = " juanito";
+		String nombreEnMayuscula = nombre.toUpperCase();
+		System.out.println("Nombre en mayuscula: "+ nombreEnMayuscula);
+		
+		String nombre2 = "JOSE";
+		String nombreEnMinuscula = nombre2.toLowerCase();
+		System.out.println("Nombre en minuscula: "+ nombreEnMinuscula);
+		
+
+	}
+
+}
