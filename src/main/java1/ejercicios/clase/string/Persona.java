b0a685416d58cb9feb49c4ef73599f783369153b
Karla Chipantasi <belenchipantai@gmail.com>
2021-09-07 20:09:52 -0500

    taller

---
+++ src/main/java1/ejercicios/clase/string/Persona.java
@@ -0,0 +1,32 @@
+package ejercicios.clase.string;
+
+public class Persona {
+	// taller
+	
+	private String cedula;
+	private String nombre;
+	
+	public static String pais="ECUADOR";
+	
+	public static String provincia ="Quito";
+	
+	
+	
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+
+}
